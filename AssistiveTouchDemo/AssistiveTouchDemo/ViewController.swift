22177671486faa334d629e111c78f64c508f4c45
KingCQ <wangchengqvan@hotmail.com>
2016-11-16 17:54:45 +0800

    beautiful  layer

--- AssistiveTouchDemo/AssistiveTouchDemo/ViewController.swift
+++ AssistiveTouchDemo/AssistiveTouchDemo/ViewController.swift
@@ -9,14 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var gradientLayer: CAGradientLayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .green
+        createGradientLayer()
         let assistiveTouch = AssistiveTouch(frame: CGRect(x: 10, y: 100, width: 56, height: 56))
         assistiveTouch.addTarget(self, action: #selector(tap(sender:)), for: .touchUpInside)
         assistiveTouch.setImage(UIImage(named: "AsstisTouch"), for: .normal)
         view.addSubview(assistiveTouch)
+    }
+        
+    func createGradientLayer() {
+        gradientLayer = CAGradientLayer()
+        gradientLayer.frame = self.view.bounds
+        gradientLayer.colors = [UIColor.red.cgColor, UIColor.yellow.cgColor]
+        self.view.layer.addSublayer(gradientLayer)
     }
     
     func tap(sender: UIButton) {
